7aefc46e84719e467a107db383bc62c02615f95e
Günther Enthaler <genthaler@users.noreply.github.com>
2016-10-10 00:43:23 +1100

    Oops, fixed tests

--- tests/Tests.elm
+++ tests/Tests.elm
@@ -2,7 +2,6 @@ module Tests exposing (..)
 
 import CucumberTest
 import Expect
-import GherkinHtmlTest
 import GherkinMdTest
 import GherkinParserTest
 import GherkinTest
@@ -15,6 +14,5 @@ all =
         [ GherkinTest.all
         , GherkinParserTest.all
         , GherkinMdTest.all
-        , GherkinHtmlTest.all
         , CucumberTest.all
         ]
