48d47f63b0b62ac89f90f5ee02d9f08e64c03d10
Günther Enthaler <genthaler@users.noreply.github.com>
2018-06-07 17:01:17 +1000

    WIP

--- html-tests/HtmlRunner.elm
+++ html-tests/HtmlRunner.elm
@@ -1,4 +1,3 @@
-<<<<<<< current
 module HtmlRunner exposing (..)
 
 {-| HOW TO RUN
@@ -20,28 +19,3 @@ main =
     ]
         |> concat
         |> Test.Runner.Html.run
-=======
-
-C:\Users\genthaler\AppData\Local\atom\app-1.27.1>"C:\Users\genthaler\AppData\Roaming\npm\\node_modules\elm-format\binaries\elm-format"   --stdin
-module HtmlRunner exposing (..)
-
-{-| HOW TO RUN
-1. ```bash
-cd tests
-elm-reactor
-```
-2. Visit http://localhost:8000/HtmlRunner.elm
--}
-
-import Test exposing (..)
-import Tests
-import Test.Runner.Html
-
-
-main : Test.Runner.Html.TestProgram
-main =
-    [ Tests.all
-    ]
-        |> concat
-        |> Test.Runner.Html.run
->>>>>>> before discard
